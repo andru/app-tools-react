de4da5d8c6d5de09ae033a87efccfcc7ad9b6553
Andru <andru@tinymighty.com>
2025-02-02 23:31:44 +0100

    Fix up back arrow

--- app/questionnaire/questionnaire.tsx
+++ app/questionnaire/questionnaire.tsx
@@ -155,7 +155,7 @@ export function Questionnaire() {
   return (
     <div className={`flex flex-col w-full items-center justify-center pt-2.5 bg-gray-100`}>
       <h3 className="heading flex items-center w-full pb-2.5">
-        <div className="back-arrow">
+        <div className="">
           <NavLink to="/">
             <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="32" height="32" viewBox="0 0 100 100">
               <path fillRule="evenodd" fill="#444" clipRule="evenodd" d="M64.274,27.414L43.2,50l21.074,22.586c1.414,1.451,1.441,3.776,0.027,5.227  c-1.414,1.451-3.681,1.424-5.095-0.027L35.753,52.652c-0.018-0.017-0.038-0.028-0.055-0.046c-0.679-0.696-1.016-1.594-1.037-2.5  c-0.001-0.035-0.009-0.071-0.009-0.106c0-0.036,0.008-0.07,0.009-0.106c0.021-0.906,0.359-1.804,1.037-2.5  c0.017-0.018,0.037-0.029,0.055-0.046l23.452-25.134c1.414-1.451,3.681-1.478,5.095-0.027  C65.715,23.638,65.688,25.963,64.274,27.414z"/>
