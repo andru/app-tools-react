a5fcfc1ffbe63702230290fed2a906da020b49b7
Andru <andru@tinymighty.com>
2025-02-06 15:13:42 +0100

    Add visible error message for TapCart initialisation

--- app/menu/menu.tsx
+++ app/menu/menu.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router';
 
 export function Menu() {
+
+  const [userError, setUserError] = useState<string | null>(null);
+  useEffect(() => {
+    if (typeof Tapcart === 'undefined') {
+      setUserError('Failed to load user data. (TC-NF)');
+    } else {
+      const tc = Tapcart;
+      if (!tc.isInitialized) {
+        setUserError('Failed to load user data. (TC-NI)');
+      } else {
+        if (!tc.variables?.customer?.id) {
+          setUserError('Failed to load user data. (TC-UNF)');
+        } else {
+          setUserError(`Failed to load user data. (TC-BU) ${tc.variables?.customer?.id}`);
+        }
+      }
+    }
+  })
+
+
   return (
     <main className="h-full flex pt-4 pb-4 bg-gray-200">
       <ul className="list-none p-5 w-full">
@@ -22,6 +43,9 @@ export function Menu() {
             <span className="w-10">+</span>
           </NavLink>
         </li>
+        {userError ? <div className="w-full p-5 bg-red-50 text-red-500 text-center">
+        {userError}
+      </div> : null}
       </ul>
     </main>
   );
